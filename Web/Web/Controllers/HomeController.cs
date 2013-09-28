b8c2e018d32942f062e5da7982c176790af3ace2
crziter <crziter@gmail.com>
2013-09-27 23:47:04 -0700

    Thêm DB, tạo bảng User và class User trong model

--- Web/Web/Controllers/HomeController.cs
+++ Web/Web/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace Web.Controllers
         {
             return View();
         }
+
         public ActionResult About()
         {
             return View();
---
+++ Web/Web/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    [Table("tblUsers")]
+    public class User
+    {
+        [Key]
+        public int Userid { get; set; }
+
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [StringLength(32)]
+        public string Password { get; set; }
+
+        [StringLength(30)]
+        public string Realname { get; set; }
+
+        [StringLength(20)]
+        public string Tel { get; set; }
+
+        [StringLength(70)]
+        public string Email { get; set; }
+    }
+}
